8d88ba70e76f04e3fb300870c084a517a203356d
FabianLugo <28177491+FabianLugo@users.noreply.github.com>
2025-01-04 03:55:59 -0600

    Create getComputerChoice function
    The game will be played against the computer,
    the function randomly returns “rock”, “paper” or “scissors”.

--- java-script.js
+++ java-script.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+console.log(getComputerChoice());
+
+
+
+
+function getComputerChoice(){
+    const paper="paper";
+    const rock ="rock";
+    const scissors ="scissors";
+
+    let randomNumber = Math.floor(Math.random()*3);
+    if(randomNumber == 1){
+        return paper;
+    }else if(randomNumber == 2){
+        return rock;
+    }else{
+        return scissors;
+    }
+}
