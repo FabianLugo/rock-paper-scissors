42dc2eeceee679d8caadd908c001938336914cef
FabianLugo <28177491+FabianLugo@users.noreply.github.com>
2025-01-04 20:31:24 -0600

    Create function playRound
    Your game will be played round by round.
    You will write a function that takes the
    human and computer player choices as arguments,
    plays a single round, increments the round
    winner’s score and logs a winner announcement.

--- java-script.js
+++ java-script.js
@@ -1,5 +1,5 @@
-humanScore = 0;
-computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
     const paper="paper";
@@ -38,3 +38,62 @@ function getHumanChoice(){
 
     return humanChoice;    
 }
+
+function playRound(humanChoice,computerChoice){
+    switch(humanChoice){
+        case "rock":{
+            switch(computerChoice){
+                case "rock":
+                    console.log("It's a tie!");
+                    break;
+                case "paper":{
+                    console.log("You lose! Paper beats Rock");
+                    computerScore++;
+                    break;}
+                case "scissors":{
+                    console.log("You win!  Rock beats Scissors");
+                    humanScore++;
+                    break;}
+            }
+            break;
+        }
+        case "paper":{
+            switch(computerChoice){
+                case "rock":{
+                    console.log("You win! Paper beats Rock");
+                    humanScore++;
+                    break;}
+                case "paper":
+                    console.log("It's a tie!");
+                    break;
+                case "scissors":{
+                    console.log("You lose! Scissors beats paper");
+                    computerScore++;
+                    break;}
+            }
+            break;
+        }
+        case "scissors":{
+            switch(computerChoice){
+                case "rock":{
+                    console.log("You lose! Rock beats Scissors");
+                    computerScore++;
+                    break;}
+                case "paper":{
+                    console.log("You win! Scissors beats paper");
+                    humanScore++;
+                    break;}
+                case "scissors":
+                    console.log("It's a tie!");
+                    break;
+            }
+            break;
+        }
+    }
+    
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection= getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
